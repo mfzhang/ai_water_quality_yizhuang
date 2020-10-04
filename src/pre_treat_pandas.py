90a3e82c1d870dc0da4f12b8ba0e28001f3a053b
maziqing.713 <maziqing.713@bytedance.com>
2020-10-05 00:39:15 +0800

    add pretreat process with pandas

--- src/pre_treat_pandas.py
+++ src/pre_treat_pandas.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2020/9/16 0:29
 # @Author  : MA Ziqing
-# @FileName: pre_processor.py
+# @FileName: pre_treat_pandas.py
 
 import numpy as np
 import pandas as pd
@@ -12,21 +12,21 @@ class PreTreatPandas(object):
         self.name = 'PreProcessor'
 
     def mask_extreme_value(self, df):
-        upper_bound = df.quantile(0.9)
-        lower_bound = df.quantile(0.1)
-        default_column = df.columns()[1]
+        default_column = df.columns[1]
+        upper_bound = df[default_column].quantile(0.9)
+        lower_bound = df[default_column].quantile(0.1)
         replace_value = df[default_column].mean()
-        df[df[default_column]>upper_bound] = replace_value
-        df[df[default_column]<lower_bound] = replace_value
+        df[df[default_column] > upper_bound] = replace_value
+        df[df[default_column] < lower_bound] = replace_value
         return df
 
     def get_the_rolling_mean(self, df):
-        df = df.rolling(5).mean().dropna()
-        return df
+        df_ = df.rolling(2).mean().dropna()
+        return df_
 
 
 def test():
-    df = pd.DataFrame({'value1': [1, 2, 3], 'value2': [2, 2, 2]})
+    df = pd.DataFrame({'value1': [1, 2, 3, 4, 5, 6], 'value2': [2]*6})
     print('the original df is {}'.format(df))
     pre_treat_pandas = PreTreatPandas()
     df = pre_treat_pandas.mask_extreme_value(df)
