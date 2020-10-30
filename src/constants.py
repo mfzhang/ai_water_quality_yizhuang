1ad4a19bb2d61584eb1edd25e79f9e43820257e2
maziqing.713 <maziqing.713@bytedance.com>
2020-10-30 23:36:58 +0800

    fix sql select

--- src/constants.py
+++ src/constants.py
@@ -12,9 +12,20 @@ class WaterQualityStandard(object):
     TRANSPARENCY = 1  # 透明度
     CONDUCTIVITY = 1  # 电导率
     TURBIDITY = 1  # 浊度
+    PH = 7  # pH
+    CHLORINE = 1  # 余氯
+    ORT = 1  # 氧化还原电位 oxidation-reduction titration
 
 
-class PhStandard(object):
-    STANDARD = 6.5
-    MAXLIMIT = 6.6
-    MINLIMIT = 6.4
+class WaterQualityMaxLim(object):
+    TRANSPARENCY = 1.1
+    CONDUCTIVITY = 1.1
+    TURBIDITY = 1.1
+    pH = 7.1
+    CHLORINE = 1.1
+    ORT = 1.1
+
+# class PhStandard(object):
+#     STANDARD = 6.5
+#     MAXLIMIT = 6.6
+#     MINLIMIT = 6.4
